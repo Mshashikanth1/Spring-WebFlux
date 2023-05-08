39f02fdb42977f1bd8744f92f968d7f8beffeb24
shashi <mallepally.varma@jsw.in>
2023-05-08 18:16:45 +0530

    facts in the range

--- src/main/java/com/example/demo/Router/coolApi.java
+++ src/main/java/com/example/demo/Router/coolApi.java
@@ -1,9 +1,13 @@
 package com.example.demo.Router;
 
 import com.example.demo.WebClient.NumbersFact;
+import com.fasterxml.jackson.annotation.JsonAlias;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+
+import java.util.Map;
 
 @RestController
 public class coolApi {
@@ -16,4 +20,9 @@ public class coolApi {
     public Mono<String> getRandomNumberFact(){
         return numbersFact.getRandomFact();
     }
+    @GetMapping(value="number/fact/{min}/{max}")
+    public Mono<String> getNumberFactsInRange(@PathVariable String min, @PathVariable String max){
+        return numbersFact.getFactsInRange(min,max);
+    }
+
 }
--- src/main/java/com/example/demo/WebClient/NumbersFact.java
+++ src/main/java/com/example/demo/WebClient/NumbersFact.java
@@ -1,17 +1,18 @@
 package com.example.demo.WebClient;
 
 import com.example.demo.DTO.Greeting;
+import com.fasterxml.jackson.annotation.JsonAlias;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+
+import java.util.Map;
+
 @Component
 public class NumbersFact {
     private final WebClient webClient;
 
-//    public NumbersFact(WebClient webClient) {
-//        this.webClient = webClient;
-//    }
 
     public NumbersFact(WebClient.Builder builder) {
         this.webClient = builder.baseUrl("http://localhost:8080").build();
@@ -19,6 +20,14 @@ public class NumbersFact {
 
     public Mono<String> getRandomFact(){
          return this.webClient.get().uri("http://numbersapi.com/random").accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+    public Mono<String> getFactsInRange(String min,String max){
+        return this.webClient.get().uri("http://numbersapi.com/"
+                        .concat(min)
+                        .concat(",")
+                        .concat(max)).accept(MediaType.APPLICATION_JSON)
                 .retrieve()
                 .bodyToMono(String.class);
     }
