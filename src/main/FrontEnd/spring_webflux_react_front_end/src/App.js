20d672b38b46486c493d4684f70b8a227e8932a0
shashi <mallepally.varma@jsw.in>
2023-05-09 11:38:23 +0530

    call api's from backend

--- src/main/FrontEnd/spring_webflux_react_front_end/src/App.js
+++ src/main/FrontEnd/spring_webflux_react_front_end/src/App.js
@@ -1,21 +1,70 @@
 import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import myLogo from './64588.jpg';
 import './App.css';
 import axios from "axios";
+import {json} from "react-router-dom";
+//import cors from "cors";
+
 
 
 function App() {
-  // axios("http://numbersapi.com/random")
-  //     .then((result) => setUsers(result.data.data))
-  //     .then((json) => {
-  //       return json.data;
-  //     })
-  //     .catch((error) => {
-  //       console.error(error);
-  //     })
-  //     .then((response) => response.parse());
+
+    //define a state variable to store the api response
+    const [apiResponse,setApiResponse]=useState('')
+
+    //make the api call and update the state variable with the reponse.
+    useEffect(() =>{
+        fetch("http://localhost:8080/random/number/fact")
+            .then(response =>response.text())
+            .then(data=>setApiResponse(data))
+            .catch(error =>console.log(error));
+    },[]);
 
 
+    //call a json api to do that we need to user useState to store data and initiali
+    const [apiResponseJson,setApiResponseJson]=useState({})
+
+    //useEffect is used to make the api call and deps are used for when to make call
+    useEffect(()=>{
+        fetch("http://localhost:8080/number/fact/1..5/10")
+            .then(response=>response.json())
+            .then(data=>setApiResponseJson(data))
+    },[]);
+
+
+
+    //  let fetchComments=async ()=>{
+    //     const  response=axios("http://localhost:8080/random/number/fact")
+    //     setComments(response.data)
+    // }
+
+
+    //
+    // const corsOrigin ={
+    //     origin:'http://localhost:8080', //or whatever port your frontend is using
+    //     credentials:true,
+    //     optionSuccessStatus:200
+    // }
+    // app.use(cors(corsOrigin));
+
+    //
+    // let request=new XMLHttpRequest();
+    // request.open("GET","http://localhost:8080/random/number/fact");
+    // request.setRequestHeader("Access-Control-Allow-Origin" ,"http://localhost:8080");
+    // request.setRequestHeader("Access-Control-Allow-Origin", "*")
+    // request.setRequestHeader("Access-Control-Allow-Credentials", "true");
+    // request.setRequestHeader("Access-Control-Max-Age", "1800");
+    // request.setRequestHeader("Access-Control-Allow-Headers", "content-type");
+    // request.send();
+    // request.onload = () => {
+    //     console.log(request);
+    //     if (request.status === 200) {
+    //         console.log(request.response);
+    //     } else {
+    //         console.log(`error ${request.status} ${request.statusText}`)
+    //     }
+    // }
 
   return (
     <div className="App">
@@ -33,10 +82,24 @@ function App() {
           Learn React
         </a>
         <p>
-          <h>Hello fun starts from hear!
+          <h>Hello fun starts from hear!:<br></br>
+              <br></br>
+              <br></br>
+              API THAT RETURN SIMPLE STRING:
+              <br></br>
+              <div style={{ backgroundColor: 'black', color: 'white', padding: '4px',fontSize: '15px' }}>
+                  {apiResponse}
+              </div>
 
+              <br></br>
+              <br></br>
+              API THAT RETURN SIMPLE JSON:
+              <div style={{ backgroundColor: 'black', color: 'white', padding: '4px',fontSize: '15px' }}>
+                  <pre>{JSON.stringify(apiResponseJson,null,2)}</pre>
+              </div>
 
           </h>
+
         </p>
       </header>
     </div>
